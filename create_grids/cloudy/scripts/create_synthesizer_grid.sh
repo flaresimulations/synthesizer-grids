948d854d953afff689ee474b331bce623f5666be
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-11-01 18:35:32 +0000

    updated scripts

--- create_grids/cloudy/scripts/create_synthesizer_grid.sh
+++ create_grids/cloudy/scripts/create_synthesizer_grid.sh
@@ -2,7 +2,15 @@
 # synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/" # SW's machine
 synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
 
-grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy-c17.03"
 
 cd ..
-python3 create_synthesizer_grid.py -grid_name $grid -synthesizer_data_dir $synthesizer_dir -line_calc_method linelist
+while IFS="" read -r p || [ -n "$p" ]
+do
+  arrIN=(${p// / })
+  grid=${arrIN[0]}
+  echo $grid
+  python3 create_synthesizer_grid.py -grid_name $grid -synthesizer_data_dir $synthesizer_dir -line_calc_method linelist
+done < scripts/grids.txt
+
+
+
