e54209adaddd04e137524253b6420fe6d235d51b
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-12-04 11:09:18 +0000

    updated apollo scripts

--- create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs.sh
+++ create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs.sh
@@ -17,4 +17,4 @@ do
   printf '%s\n' "$params"
   echo python3 create_cloudy_input_grid.py -synthesizer_data_dir $synthesizer_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
   python3 create_cloudy_input_grid.py -synthesizer_data_dir $synthesizer_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
-done < apollo_production_inputs/incident.txt
+done < apollo_production_scripts/incident.txt
