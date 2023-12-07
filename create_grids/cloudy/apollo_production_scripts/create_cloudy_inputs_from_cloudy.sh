4e51acf692c955d76de5ea517170cedbaecd34aa
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-12-07 16:16:33 +0000

    updated scripts

--- create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs_from_cloudy.sh
+++ create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs_from_cloudy.sh
@@ -7,7 +7,7 @@ synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
 machine="apollo"
 incident_cloudy_model="blackbody"
 c="/research/astro/flare/software/cloudy/"
-cloudy_params="c17.03-limited" 
+cloudy_params="c17.03-nlr" 
 
 cd ..
 python create_cloudy_input_grid_from_cloudy.py -synthesizer_data_dir $synthesizer_dir -machine $machine -incident_cloudy_model $incident_cloudy_model  -cloudy_params $cloudy_params  -cloudy_path $c
