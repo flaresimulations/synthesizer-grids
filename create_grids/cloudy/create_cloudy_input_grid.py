931f1b10cc586db34d89ba6736d0303123ba42de
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-01-09 15:22:38 +0000

    bug fix

--- create_grids/cloudy/create_cloudy_input_grid.py
+++ create_grids/cloudy/create_cloudy_input_grid.py
@@ -157,11 +157,11 @@ if __name__ == "__main__":
 
     # open the new grid
     with h5py.File(
-        f"{args.synthesizer_data_dir}/{new_grid_name}.hdf5", "w"
+        f"{args.grid_dir}/{new_grid_name}.hdf5", "w"
     ) as hf:
         # open the original incident model grid
         with h5py.File(
-            f"{args.synthesizer_data_dir}/{args.incident_grid}.hdf5", "r"
+            f"{args.grid_dir}/{args.incident_grid}.hdf5", "r"
         ) as hf_incident:
             if verbose:
                 hf_incident.visit(print)
