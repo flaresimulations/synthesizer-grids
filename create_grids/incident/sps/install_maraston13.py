f905b2e2f4a8e486eb809b20e18870a2c9e3aeec
wjr21 <wjr21@sussex.ac.uk>
2024-01-09 14:45:57 +0000

    Formatting latest again

--- create_grids/incident/sps/install_maraston13.py
+++ create_grids/incident/sps/install_maraston13.py
@@ -11,7 +11,11 @@ import sys
 
 # Allow the file to use incident_utils
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from incident_utils import write_data_h5py, write_attribute, add_log10Q  # , __tag__
+from incident_utils import (
+    write_data_h5py,
+    write_attribute,
+    add_log10Q,
+)  # , __tag__
 
 
 def make_grid(model, imf, output_dir):
@@ -22,7 +26,7 @@ def make_grid(model, imf, output_dir):
             dictionary containing model parameters
         imf (string):
             Initial mass function, Salpeter or
-            Kroupa 
+            Kroupa
         output_dir (string):
             directory where the raw Maraston+13 files are read from
     Returns:
@@ -33,12 +37,16 @@ def make_grid(model, imf, output_dir):
     # define output
     fname = f"{synthesizer_data_dir}/{model_name}/{model_name}_{imf}.hdf5"
 
-    metallicities = np.array([0.01,0.001,0.02,0.04])  # array of available metallicities
+    metallicities = np.array(
+        [0.01, 0.001, 0.02, 0.04]
+    )  # array of available metallicities
 
-    metallicity_code = {0.01: "001",
-                        0.001: "0001",
-                        0.02: "002",
-                        0.04: "004"}  # codes for converting metallicty
+    metallicity_code = {
+        0.01: "001",
+        0.001: "0001",
+        0.02: "002",
+        0.04: "004",
+    }  # codes for converting metallicty
 
     # open first raw data file to get age
     fn = f"{output_dir}/sed_M13.{imf_code[imf]}z{metallicity_code[metallicities[0]]}"
@@ -46,10 +54,10 @@ def make_grid(model, imf, output_dir):
     ages_, _, lam_, llam_ = np.loadtxt(fn).T  # llam is in (ergs /s /AA /Msun)
 
     ages_Gyr = np.sort(np.array(list(set(ages_))))  # Gyr
-    ages = ages_Gyr * 1e9 * yr 
+    ages = ages_Gyr * 1e9 * yr
     log10ages = np.log10(ages)
 
-    lam = lam_[ages_ == ages_[0]] * Angstrom 
+    lam = lam_[ages_ == ages_[0]] * Angstrom
 
     spec = np.zeros((len(ages), len(metallicities), len(lam)))
 
@@ -66,7 +74,10 @@ def make_grid(model, imf, output_dir):
     # write out spectra
     write_data_h5py(fname, "spectra/wavelength", data=lam, overwrite=True)
     write_attribute(
-        fname, "spectra/wavelength", "Description", "Wavelength of the spectra grid"
+        fname,
+        "spectra/wavelength",
+        "Description",
+        "Wavelength of the spectra grid",
     )
     write_attribute(fname, "spectra/wavelength", "Units", "AA")
 
@@ -84,11 +95,16 @@ def make_grid(model, imf, output_dir):
 
     write_data_h5py(fname, "axes/log10age", data=log10ages, overwrite=True)
     write_attribute(
-        fname, "axes/log10age", "Description", "Stellar population ages in log10 years"
+        fname,
+        "axes/log10age",
+        "Description",
+        "Stellar population ages in log10 years",
     )
     write_attribute(fname, "axes/log10age", "Units", "log10(yr)")
 
-    write_data_h5py(fname, "axes/metallicity", data=metallicities, overwrite=True)
+    write_data_h5py(
+        fname, "axes/metallicity", data=metallicities, overwrite=True
+    )
     write_attribute(fname, "axes/metallicity", "Description", "raw abundances")
     write_attribute(fname, "axes/metallicity", "Units", "dimensionless [Z]")
 
@@ -97,9 +113,13 @@ def make_grid(model, imf, output_dir):
 
 # Lets include a way to call this script not via an entry point
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Maraston+13 download and grid creation")
+    parser = argparse.ArgumentParser(
+        description="Maraston+13 download and grid creation"
+    )
     parser.add_argument("-synthesizer_data_dir", type=str, required=True)
-    parser.add_argument("-download_data", "--download_data", type=bool, default=False)
+    parser.add_argument(
+        "-download_data", "--download_data", type=bool, default=False
+    )
 
     args = parser.parse_args()
 
@@ -119,7 +139,6 @@ if __name__ == "__main__":
     }  #'synthesizer-grids_tag': __tag__,
 
     for imf in imfs:
-
         fname = make_grid(
             model, imf, output_dir
         )  # makes the grid and returns the name
