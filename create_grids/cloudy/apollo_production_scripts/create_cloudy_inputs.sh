528402aa2ba10ba9a1d5519d21f5fc7bf81bcd07
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-01-11 10:38:48 +0000

    updated apollo production scripts

--- create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs.sh
+++ create_grids/cloudy/apollo_production_scripts/create_cloudy_inputs.sh
@@ -4,6 +4,8 @@
 
 # synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/"
 synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
+grid_dir=$synthesizer_dir/grids/dev
+cloudy_dir=$synthesizer_dir/cloudy/
 machine="apollo"
 c="/research/astro/flare/software/cloudy/"
 
@@ -15,6 +17,6 @@ do
   params=${arrIN[1]}
   printf '%s\n' "$sps"
   printf '%s\n' "$params"
-  echo python3 create_cloudy_input_grid.py -synthesizer_data_dir $synthesizer_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
-  python3 create_cloudy_input_grid.py -synthesizer_data_dir $synthesizer_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
+  echo create_cloudy_input_grid.py -grid_dir $grid_dir -cloudy_dir $cloudy_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
+  python3 create_cloudy_input_grid.py -grid_dir $grid_dir -cloudy_dir $cloudy_dir -machine $machine -incident_grid $incident  -cloudy_params $params  -cloudy_path $c
 done < apollo_production_scripts/incident.txt
