14d3f270daa92b39a9447966ea17f540d6348817
wjr21 <wjr21@sussex.ac.uk>
2024-01-16 15:41:28 +0000

    Improved parser, now directly inherits from argparse

--- src/synthesizer_grids/utilities/parser.py
+++ src/synthesizer_grids/utilities/parser.py
@@ -10,9 +10,12 @@ Example Usage:
 import argparse
 
 
-class Parser:
+class Parser(argparse.ArgumentParser):
     """
     A class for parsing command line arguments in a standardised way.
+
+    NOTE: This class is a wrapper around the argparse.ArgumentParser class
+    adding some standardised arguments and quality of life methods.
 
     Attributes:
         parser (argparse.ArgumentParser):
@@ -30,10 +33,10 @@ class Parser:
                 Should the parser accept alpha arguments?
         """
         # Create the parser
-        self.parser = argparse.ArgumentParser(description=description)
+        super(Parser, self).__init__(description=description)
 
         # Add filepath argument
-        self.parser.add_argument(
+        self.add_argument(
             "--synthesizer-data-dir",
             "-s",
             type=str,
@@ -42,7 +45,7 @@ class Parser:
         )
 
         # Add download flag
-        self.parser.add_argument(
+        self.add_argument(
             "--download",
             "-d",
             action="store_true",
@@ -53,33 +56,9 @@ class Parser:
         if with_alpha:
             self._add_alpha_args()
 
-    def parse(self):
-        """
-        Parse the command line arguments.
-
-        Returns:
-            argparse.Namespace:
-                The parsed arguments.
-        """
-        return self.parser.parse_args()
-
-    def add_argument(self, *args, **kwargs):
-        """
-        Add an argument to the parser.
-
-        Args:
-            *args:
-                Positional arguments to pass to the parser.
-            **kwargs:
-                Keyword arguments to pass to the parser.
-        """
-        self.parser.add_argument(*args, **kwargs)
-
     def _add_alpha_args(self):
-        """
-        Add arguments for alpha enhancement.
-        """
-        self.parser.add_argument(
+        """Add arguments for alpha enhancement."""
+        self.add_argument(
             "--inidividual",
             "-i",
             action="store_true",
@@ -88,10 +67,14 @@ class Parser:
                 "alpha enhancements?"
             ),
         )
-
-        self.parser.add_argument(
+        self.add_argument(
             "--full",
             "-f",
             action="store_true",
             help="Should a 3D grid be made with an alpha enhancement axis?",
         )
+
+    def __str__(self):
+        """Summarise the command line arguments."""
+        self.print_help()
+        return ""
