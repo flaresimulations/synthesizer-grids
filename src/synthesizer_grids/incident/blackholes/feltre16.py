5363bf50bb43fd3b2830d7cb39f7b387c8978672
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-02-06 14:41:36 +0000

    updated feltre16 model to use new infrastructure

--- src/synthesizer_grids/incident/blackholes/feltre16.py
+++ src/synthesizer_grids/incident/blackholes/feltre16.py
@@ -1,98 +1,128 @@
 """
-Create a synthesizer incident grid for a broken power-law SED model.
+Create a synthesizer incident grid for a broken power-law SED model using
+the Feltre+2016 approach.
 """
-
-import h5py
 import numpy as np
 from unyt import c, Angstrom
-from utils import (
-    __tag__,
-    broken_power_law,
-    add_log10_specific_ionising_lum,
-)
-from datetime import date
-
-synthesizer_data_dir = "/Users/sw376/Dropbox/Research/data/synthesizer/"
-
-model_name = "feltre16"
+from synthesizer_grids.parser import Parser
+from synthesizer_grids.grid_io import GridFile
 
 
-# axes = ['alpha', 'metallicity']
-axes = ["alpha"]
+def broken_power_law(x, edges, indices, normalisations=False, normalise=True):
+    """
+    Function to produce a broken power-law spectrum.
 
-axes_descriptions = {}
-axes_descriptions["alpha"] = "X-ray - UV power-law slope"
-# axes_descriptions['metallicity'] = 'gas phase metallicity'
+    Arguments
+        x (array, float)
+            x-coordinate
+        edges (list, float)
+            List of floats describing the edges of the individual power-law
+            components.
+        indices (list, float)
+            List of the power-law indices. Length should be one less than
+            edges.
+        normalisations (list, float)
+            List of normalisations. Length should be one less than
+            edges.
+        normalise (bool)
+            Flag to decide whether to normalise to unity.
 
-axes_units = {}
-axes_units["alpha"] = "dimensionless"
-# axes_units['metallicity'] = 'dimensionless'
+    """
 
-axes_values = {}
-axes_values["alpha"] = np.arange(-2.0, -1.0, 0.2)
-# axes_values['metallicity'] = [0.00001, 0.00003, 0.0001,
-#                               0.0003, 0.001, 0.003, 0.01, 0.03]
-# axes_values['alpha'] = [-2.0]
+    y = np.zeros(len(x))
 
-# the shape of the grid (useful for creating outputs)
-axes_shape = list([len(axes_values[axis]) for axis in axes])
-print(axes_shape)
+    # if normalisations not calculated, go ahead an
+    if not normalisations:
+        normalisations = [
+            1.0,
+        ]
 
-# define edges
-edges_lam = [10.0, 2500.0, 100000.0, 1000000.0] * Angstrom  # Angstrom
-edges_nu = c / edges_lam
-edges = edges_nu.to("THz").value
+        # calcualte remaining normalisations
+        for i, (edge, ind1, ind2) in enumerate(
+            zip(edges[1:], indices, indices[1:])
+        ):
+            norm = normalisations[i] * (edge**ind1) / (edge**ind2)
+            normalisations.append(norm)
 
-# define wavelength and frequency grid
-lam = np.arange(edges_lam.value[0], edges_lam.value[-1], 10.0) * Angstrom
-nu = c / lam
-x = nu.to("THz").value
+    # now construct spectra
+    for e1, e2, ind, norm in zip(edges[0:],
+                                 edges[1:],
+                                 indices,
+                                 normalisations):
+        # identify indices within the wavelength range
 
-# define indices
-indices = [None, -0.5, 2.0]
+        s = (x >= e1) & (x < e2)
+
+        y[s] = norm * x[s] ** ind
+
+    # normalise
+    if normalise:
+        y /= np.trapz(y[::-1], x=x[::-1])
+
+    return y
 
 
-# filename
-filename = f"{synthesizer_data_dir}/grids/dev/{model_name}.hdf5"
+if __name__ == "__main__":
+    # Set up the command line arguments
+    parser = Parser(description="Feltre et al. (2016) AGN model creation.")
+    
+    # path to grid directory (i.e. where incident and new grids are stored)
+    # ** this can be replaced once Parser is updated. **
+    parser.add_argument("-grid_dir", type=str, required=True)
 
-# open the new grid
-with h5py.File(filename, "w") as hf:
-    # save model attributes
-    hf.attrs["model"] = model_name
-    hf.attrs["synthesizer-grids_tag"] = __tag__
-    hf.attrs["date"] = str(date.today())
-    hf.attrs["axes"] = axes
+    # get the arguments
+    args = parser.parse_args()
 
-    # save axis quantities
-    for axis in axes:
-        hf[f"axes/{axis}"] = axes_values[axis]
-        hf[f"axes/{axis}"].attrs["Description"] = axes_descriptions[axis]
-        hf[f"axes/{axis}"].attrs["Units"] = axes_units[axis]
+    # Define the model name
+    model = "feltre16"
+
+    # Define the grid filename and path
+    out_filename = f"{args.grid_dir}/{model}"
+
+    alphas = np.arange(-2.0, -1.0, 0.2)
+    axes = [alphas]
+
+    # the shape of the grid (useful for creating outputs)
+    axes_shape = list([len(axis) for axis in axes])
+
+    # define edges
+    edges_lam = [10.0, 2500.0, 100000.0, 1000000.0] * Angstrom  # Angstrom
+    edges_nu = c / edges_lam
+    edges = edges_nu.to("THz").value
+
+    # define wavelength and frequency grid using the edges
+    lam = np.arange(edges_lam.value[0], edges_lam.value[-1], 10.0) * Angstrom
+    nu = c / lam
+    x = nu.to("THz").value
+
+    # define indices. The first entry is None because this is set by the axis.
+    indices = [None, -0.5, 2.0]
 
     # create empty spectra grid
     spec = np.zeros((*axes_shape, len(lam)))
 
-    for i, alpha in enumerate(axes_values["alpha"]):
+    # loop over alphas
+    for i, alpha in enumerate(alphas):
+
+        # use the value of alpha to fill in the missing power-law index
         indices[0] = alpha
-        lnu = broken_power_law(x, edges[::-1], indices[::-1])
-        spec[i] = lnu
 
-        # for j, metallicity in enumerate(axes_values['metallicity']):
-        #     spec[i, j] = lnu
-
-    # save wavelength dataset
-    hf["spectra/wavelength"] = lam
-    hf["spectra/wavelength"].attrs[
-        "Description"
-    ] = "Wavelength of the spectra grid"
-    hf["spectra/wavelength"].attrs["Units"] = "Angstrom"
-
-    # save incident spectrum grid
-    hf["spectra/incident"] = spec
-    hf["spectra/incident"].attrs["Description"] = "Incident spectrum grid"
-    hf["spectra/incident"].attrs["Units"] = "erg/s/Hz"
+        # generate the broken power-law spectra
+        spec[i] = broken_power_law(x, edges[::-1], indices[::-1]) 
 
 
-# calcualte log10_specific_ionising_lum
+    # Create the GridFile ready to take outputs
+    out_grid = GridFile(out_filename, mode="w", overwrite=True)
 
-add_log10_specific_ionising_lum(filename)
+    # Write everything out thats common to all models
+    out_grid.write_grid_common(
+        model=model,
+        axes={"alpha": alphas},
+        wavelength=lam,
+        spectra={"incident": spec * erg / s / Hz},
+        alt_axes=("alphas"),
+    )
+
+    # Include the specific ionising photon luminosity
+    print("Calculating and saving specific ionising luminosity")
+    out_grid.add_specific_ionising_lum()
