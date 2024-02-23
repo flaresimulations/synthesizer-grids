65e15759113be8e6de0f9f4603e2b46e6f230621
Sophie Newman <snewman@login1.pri.sciama3.alces.network>
2024-02-23 09:42:37 +0000

    adjusting parser, readding previous fixes

--- src/synthesizer_grids/incident/sps/install_maraston13.py
+++ src/synthesizer_grids/incident/sps/install_maraston13.py
@@ -10,7 +10,7 @@ from synthesizer_grids.parser import Parser
 from synthesizer_grids.grid_io import GridFile
 
 
-def make_grid(model, imf, output_dir):
+def make_grid(model, imf, output_dir, grid_dir):
     """Main function to convert Maraston 2013 and
     produce grids used by synthesizer
     Args:
@@ -21,21 +21,23 @@ def make_grid(model, imf, output_dir):
             Kroupa
         output_dir (string):
             directory where the raw Maraston+13 files are read from
+        grid_dir (string):
+            directory where the grids are created.
     Returns:
         fname (string):
             output filename
     """
 
     # define output
-    out_filename = f"{synthesizer_data_dir}/grids/{model_name}_{imf}.hdf5"
+    out_filename = f"{grid_dir}/{sps_name}_{imf}.hdf5"
 
     metallicities = np.array(
-        [0.01, 0.001, 0.02, 0.04]
+        [0.001, 0.01, 0.02, 0.04]
     )  # array of available metallicities
 
     metallicity_code = {
-        0.01: "001",
         0.001: "0001",
+        0.01: "001",
         0.02: "002",
         0.04: "004",
     }  # codes for converting metallicty
@@ -59,6 +61,7 @@ def make_grid(model, imf, output_dir):
     # at each point in spec convert the units
     for imetal, metallicity in enumerate(metallicities):
         for ia, age_Gyr in enumerate(ages_Gyr):
+            fn = f"{output_dir}/sed_M13.{imf_code[imf]}z{metallicity_code[metallicity]}"
             print(imetal, ia, fn)
             ages_, _, lam_, llam_ = np.loadtxt(fn).T
 
@@ -84,22 +87,30 @@ if __name__ == "__main__":
     parser = Parser(description="Maraston+13 download and grid creation")
     args = parser.parse_args()
 
-    # Unpack the arguments
-    synthesizer_data_dir = args.synthesizer_data_dir
-    grid_dir = f"{synthesizer_data_dir}/grids"
+    parser.add_argument("-output_dir", type=str, required=True)
+    parser.add_argument("-grid_dir", type=str, required=True)
+    parser.add_argument("-download_data", "--download_data", type=bool, default=False)
+
+    args = parser.parse_args()
+
+    grid_dir = args.grid_dir
 
     # Define the model metadata
-    model_name = "maraston13"
+    sps_name = "maraston13"
     imfs = ["salpeter", "kroupa"]
     imf_code = {"salpeter": "ss", "kroupa": "kr"}
     model = {
-        "sps_name": "maraston",
+        "sps_name": sps_name,
         "sps_version": False,
         "alpha": False,
     }
 
-    # The location to untar the original data
-    output_dir = f"{synthesizer_data_dir}/original_data/{model_name}"
+    output_dir = args.output_dir
+    output_dir += f'/{sps_name}'
+
+    # create directory to store downloaded output if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
     for imf in imfs:
-        make_grid(model, imf, output_dir)
+        make_grid(model, imf, output_dir, grid_dir)
