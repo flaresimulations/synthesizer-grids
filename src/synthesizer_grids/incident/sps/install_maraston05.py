1e4ad056e45d7792a96ac7f7f6d254ba055521ed
Sophie <sophie.newman@port.ac.uk>
2024-09-05 10:13:13 +0100

    add changes to m05

--- src/synthesizer_grids/incident/sps/install_maraston05.py
+++ src/synthesizer_grids/incident/sps/install_maraston05.py
@@ -1,5 +1,5 @@
 """
-Download BC03 and convert to HDF5 synthesizer grid.
+Download M05 and convert to HDF5 synthesizer grid.
 """
 
 import os
@@ -9,7 +9,7 @@ from pathlib import Path
 import numpy as np
 import wget
 from synthesizer.conversions import flam_to_fnu
-from unyt import angstrom, cm, erg, s
+from unyt import angstrom, cm, dimensionless, erg, s
 from utils import get_model_filename
 
 from synthesizer_grids.grid_io import GridFile
@@ -67,7 +67,6 @@ def make_grid(model, imf, hr_morphology, input_dir, grid_dir):
 
     ages_Gyr = np.sort(np.array(list(set(ages_))))  # Gyr
     ages = ages_Gyr * 1e9  # yr
-    log10ages = np.log10(ages)
 
     lam = lam_[ages_ == ages_[0]] * angstrom
 
@@ -87,13 +86,19 @@ def make_grid(model, imf, hr_morphology, input_dir, grid_dir):
     # Create the GridFile ready to take outputs
     out_grid = GridFile(out_filename, mode="w", overwrite=True)
 
+    # A dictionary with Boolean values for each axis, where True
+    # indicates that the attribute should be interpolated in
+    # logarithmic space.
+    log_on_read = {"ages": True, "metallicities": False}
+
     # Write everything out thats common to all models
     out_grid.write_grid_common(
         model=model,
-        axes={"log10age": log10ages, "metallicity": metallicities},
+        axes={"ages": ages, "metallicity": metallicities * dimensionless},
         wavelength=lam,
         spectra={"incident": spec},
-        alt_axes=("log10ages", "metallicities"),
+        alt_axes=("ages", "metallicities"),
+        log_on_read=log_on_read,
     )
 
     # Include the specific ionising photon luminosity
