949f96ba9ff96e66ac1088c192a5063eebfbffc8
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-02-21 11:32:54 +0000

    ruff

--- src/synthesizer_grids/incident/create_reduced_grid.py
+++ src/synthesizer_grids/incident/create_reduced_grid.py
@@ -4,7 +4,8 @@ to a specific set or a max age.
 
 Example:
     python create_reduced_grid.py --grid-dir path/to/grids \
-        --original-grid bpass-2.2.1-bin_chabrier03-0.1,300.0 --new-ages=6.,7.,8.
+        --original-grid bpass-2.2.1-bin_chabrier03-0.1,300.0 \
+        --new-ages=6.,7.,8.
     python create_reduced_grid.py -grid_dir grids \
         -original_grid bpass-2.2.1-bin_chabrier03-0.1,300.0 \
         -ages=6.,7.,8, -metallicities=0.001,0.0
